b8a877111f2c55de17e8fde6a716304c3ede8d7c
Sandaru Tharuneth <sandarutharuneth@gmail.com>
2023-08-25 08:20:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ STARTUP_LOGO = r"""
 ---------------------------------------------------------------- 
 """
 
-def add_watermark(input_path, output_path, watermark_path):
+def add_watermark(input_path, output_path, watermark_path, watermark_width_factor, watermark_height_factor, watermark_opacity, watermark_position):
     watermark = Image.open(watermark_path)
     
-    # Calculate new dimensions for the watermark image (0.8X)
-    new_width = int(watermark.width * 0.8)
-    new_height = int(watermark.height * 0.8)
+    # Calculate new dimensions for the watermark image
+    new_width = int(watermark.width * watermark_width_factor)
+    new_height = int(watermark.height * watermark_height_factor)
     watermark = watermark.resize((new_width, new_height))
     
     watermark = watermark.convert("RGBA")
@@ -28,13 +28,22 @@ def add_watermark(input_path, output_path, watermark_path):
     for x in range(watermark.width):
         for y in range(watermark.height):
             r, g, b, a = watermark.getpixel((x, y))
-            watermark_with_opacity.putpixel((x, y), (r, g, b, int(a * 0.7)))
+            watermark_with_opacity.putpixel((x, y), (r, g, b, int(a * watermark_opacity)))
 
     for image_file in os.listdir(input_path):
         if image_file.lower().endswith(('.png', '.jpg', '.jpeg', '.gif')):
             image_path = os.path.join(input_path, image_file)
             image = Image.open(image_path)
-            watermark_position = (image.width - watermark_with_opacity.width, image.height - watermark_with_opacity.height)
+            if watermark_position == "TOP_RIGHT":
+                watermark_position = (image.width - watermark_with_opacity.width, 0)
+            elif watermark_position == "BOTTOM_RIGHT":
+                watermark_position = (image.width - watermark_with_opacity.width, image.height - watermark_with_opacity.height)
+            elif watermark_position == "TOP_LEFT":
+                watermark_position = (0, 0)
+            elif watermark_position == "BOTTOM_LEFT":
+                watermark_position = (0, image.height - watermark_with_opacity.height)
+            elif watermark_position == "CENTER":
+                watermark_position = ((image.width - watermark_with_opacity.width) // 2, (image.height - watermark_with_opacity.height) // 2)
             image.paste(watermark_with_opacity, watermark_position, watermark_with_opacity)
             output_file = os.path.join(output_path, image_file)
             image.save(output_file)
@@ -51,13 +60,13 @@ if __name__ == "__main__":
     input_folder = "data"
     output_folder = "watermarked_images"
     watermark_path = "static/watermark.png"
-
+    watermark_width_factor = 0.8  # Adjust the watermark width (0.8 means 80%)
+    watermark_height_factor = 0.8  # Adjust the watermark height (0.8 means 80%)
+    watermark_opacity = 0.7  # Adjust the watermark opacity (0.7 means 70%)
+    watermark_position = "TOP_LEFT"  # Change this to the desired position
+    
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
 
     print(RED_COLOR + STARTUP_LOGO + RESET_COLOR)
-    add_watermark(input_folder, output_folder, watermark_path)
-
-# ©️ Copyright Project Razer LLC 2023 All Rights Reserved.
-# Credits: @sandarutharuneth
-# License: CC0 1.0 Universal
+    add_watermark(input_folder, output_folder, watermark_path, watermark_width_factor, watermark_height_factor, watermark_opacity, watermark_position)
